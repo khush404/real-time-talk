b180567b4ea29cbf24d3aedda54fb9c28d6f3d4f
khush404 <ckhushboo483@gamil.com>
2026-09-06 16:18:54 +0530

    Added Bcrpyt

--- src/main/java/com/example/realtimetalk/entity/User.java
+++ src/main/java/com/example/realtimetalk/entity/User.java
@@ -1,6 +1,7 @@
 package com.example.realtimetalk.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 
 @Entity
@@ -18,7 +19,7 @@ public class User {
     private String email;
 
     @Column(nullable = false)
-    @JsonIgnore
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
     public User(){
