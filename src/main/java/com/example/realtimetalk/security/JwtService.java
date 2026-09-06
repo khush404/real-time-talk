f462655415982249ff4157741bb1fd4430f16e66
khush404 <ckhushboo483@gamil.com>
2026-09-06 17:14:35 +0530

    Added JWTService

---
+++ src/main/java/com/example/realtimetalk/security/JwtService.java
@@ -0,0 +1,43 @@
+package com.example.realtimetalk.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+public class JwtService {
+    private static final String SECRET_KEY =
+            "RealTimeTalkSecretKeyForJWTAuthentication123456789";
+
+    private final SecretKey key = Keys.hmacShaKeyFor(
+            SECRET_KEY.getBytes(StandardCharsets.UTF_8)
+    );
+
+    private final long expirationTime = 1000*60*60;
+
+    public String generateToken(String username){
+        Date now = new Date();
+        Date expiration = new Date(
+                now.getTime() + expirationTime
+        );
+
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(now)
+                .expiration(expiration)
+                .signWith(key)
+                .compact();
+    }
+
+    public String extractUsername(String token){
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+
+    }
+}
